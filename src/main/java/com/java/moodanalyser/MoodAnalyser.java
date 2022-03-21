1143fe3f915f4c638836e43f130de1e726166cd0
kumudinidesale <kdesale1211@gmail.com>
2022-03-21 11:28:29 +0530

    added:Handle exception if user provides invalid mood

--- src/main/java/com/java/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/java/moodanalyser/MoodAnalyser.java
@@ -2,10 +2,23 @@ package com.java.moodanalyser;
 
 public class MoodAnalyser {
 
-	public String analyseMood(String msg) {
-		if (msg.contains("Sad")) {
-			return "Sad";
+	private String msg;
+
+	public MoodAnalyser() {
+	}
+
+	public MoodAnalyser(String msg) {
+		this.msg = msg;
+	}
+
+	public String analyzeMood() {
+		try {
+			if (msg.contains("Sad")) {
+				return "Sad";
+			}
+			return "Happy";
+		} catch (NullPointerException e) {
+			return "Happy";
 		}
-		return "Happy";
 	}
 }
