385149b9dfacf4487c607c9ef365481620ed3dee
kumudinidesale <kdesale1211@gmail.com>
2022-03-21 11:18:20 +0530

    added:Refactored the code

---
+++ src/test/java/com/java/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,22 @@
+package com.java.moodanalyser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+//Ability to analyse and respond Happy or Sad Mood
+
+public class MoodAnalyserTest {
+	@Test
+	public void givenMessage_WhenSad_ShouldReturnSad() {
+		MoodAnalyser moodAnalyzer = new MoodAnalyser("I am in Sad Mood");
+		String mood = moodAnalyzer.analyzeMood();
+		Assert.assertEquals("Sad", mood);
+	}
+
+	@Test
+	public void givenMessage_WhenHappy_ShouldReturnHappy() {
+		MoodAnalyser moodAnalyzer = new MoodAnalyser("I am in Happy Mood");
+		String mood = moodAnalyzer.analyzeMood();
+		Assert.assertEquals("Happy", mood);
+	}
+}
